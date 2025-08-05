99e96318cd8a4b7190d967a1b73d2c1de2d3eca5
Sophia M <sophia.moerchen@gmail.com>
2025-08-05 16:08:45 +0200

    Ajout du calcul votes / total users * 100 pour le $resultPercent ainsi que l'ajout du ROUND($resultPercent, 2)

---
+++ login.php
@@ -0,0 +1,12 @@
+<?php require_once 'lib/required_files.php';
+
+
+
+
+require_once 'templates/header.php';
+?>
+
+
+
+
+<?php require_once 'templates/footer.php' ?>
--- sondage.php
+++ sondage.php
@@ -4,22 +4,20 @@ require_once 'lib/poll.php';
 
 $error_404 = false;
 
-if (isset($_GET['id'])){
+if (isset($_GET['id'])) {
     // /!\  type STRING à changer car méthode getPollById attend du INT cela s'appelle un "cast"
-    $id = (int)$_GET['id'];
+    $id = (int) $_GET['id'];
     // On veut dabord voir si le sondage existe avant d'appeler les résultats du sondage que nous voulons afficher sur la page.
     $poll = getPollById($pdo, $id);
-    if($poll){
+    if ($poll) {
         $pageTitle = $poll['title'];
         $results = getPollResultsByPollId($pdo, $id);
-
-
-    getPollTotalUsersByPollId($pdo, $id);  
+        $totalUsers = getPollTotalUsersByPollId($pdo, $id);
     } else {
         $error_404 = true;
         // ou bien : header("Location: page.php");exit(); --> ⚠️ exit(); est fortement recommandé juste après pour éviter d’exécuter du code en trop.
     }
-    
+
 } else {
     $error_404 = true;
 }
@@ -28,25 +26,32 @@ if (isset($_GET['id'])){
 require_once 'templates/header.php';
 
 
-if(!$error_404) {
-?>
+if (!$error_404) {
+    ?>
 <div class="row align-items-center g-5 py-5">
     <div class="col-lg-6">
-        <h1 class="display-5 fw-bold lh-1 mb-3"><?=$poll['title']?></h1>
-        <p class="lead"><?=$poll['description']?>
+        <h1 class="display-5 fw-bold lh-1 mb-3"><?= $poll['title'] ?></h1>
+        <p class="lead"><?= $poll['description'] ?>
         </p>
     </div>
     <div class="col-lg-6">
         <h2>Résultats</h2>
         <div class="results">
-            <?php foreach($results as $index => $result){?>
-            <h3><?= $result['name']?> </h3>
-
-            <div class="progress " role="progressbar" aria-label="Example with label" aria-valuenow="25"
-                aria-valuemin="0" aria-valuemax="100">
-                <div class="progress-bar progress-bar-striped progress-color-<?= $index?>" style="width: 25%">
-                    <?= $result['name']?>
-                    25%
+            <?php foreach ($results as $index => $result) {
+                    if ($totalUsers) {
+                        // calcul
+                        $resultPercent = $result['votes'] / $totalUsers * 100;
+                    } else {
+                        $resultPercent = 0;
+                    }
+                    ?>
+            <h3><?= $result['name'] ?> </h3>
+            <div class="progress " role="progressbar" aria-label="<?= $result['name']; ?>"
+                aria-valuenow="<?= $resultPercent; ?>" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar progress-bar-striped progress-color-<?= $index ?>"
+                    style="width: <?= $resultPercent; ?>%">
+                    <?= $result['name']; ?>
+                    <?= round($resultPercent, 2); ?>%
                 </div>
             </div>
             <?php } ?>
@@ -58,7 +63,7 @@ if(!$error_404) {
 <?php } else {
     ?>
 <h1>Ce sondage n'existe pas</h1>
-<?php }?>
+<?php } ?>
 
 
 
@@ -68,4 +73,4 @@ if(!$error_404) {
 
 
 
-<?php require_once 'templates/footer.php'?>
+<?php require_once 'templates/footer.php' ?>
